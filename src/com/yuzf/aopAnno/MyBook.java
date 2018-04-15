b2e400efba6594774ac0280c098f2cd2a7d09ac0
zjzjyuzf <zj_zj_yuzf@163.com>
2018-04-15 23:29:08 +0800

    update------2

--- src/com/yuzf/aopAnno/MyBook.java
+++ src/com/yuzf/aopAnno/MyBook.java
@@ -1,5 +1,7 @@
 package com.yuzf.aopAnno;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -11,4 +13,10 @@ public class MyBook {
 		System.out.println("before...");
 	}
 	
+	@Around(value="execution(* com.yuzf.aopAnno.Book.*(..))")
+	public void around(ProceedingJoinPoint joinPoint) throws Throwable {
+		System.out.println("around!!!");
+		joinPoint.proceed();
+		System.out.println("around???");
+	}
 }
